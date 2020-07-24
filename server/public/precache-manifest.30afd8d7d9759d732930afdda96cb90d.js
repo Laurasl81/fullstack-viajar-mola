3397f772da778af21ab09ee35dd7e5470cb30b92
Laura Sanchez <laura695hotmail.com>
2020-07-24 21:11:06 +0200

    Deploying

---
+++ server/public/precache-manifest.30afd8d7d9759d732930afdda96cb90d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f32bc30ba514ed0ccc02718419f16d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e846a58b193397832885",
+    "url": "/static/css/main.7369d2e5.chunk.css"
+  },
+  {
+    "revision": "6f076fd48248506dca18",
+    "url": "/static/js/2.06671d7f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.06671d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e846a58b193397832885",
+    "url": "/static/js/main.17deff2c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
